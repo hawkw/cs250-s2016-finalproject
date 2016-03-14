ad2e2f614e7cfb8ce12e5d4c75b46499e18bcd6c
Hawk Weisman <hi@hawkweisman.me>
2016-03-14 10:38:15 -0400

    [Measure] More work on Measure

--- src/main/scala/edu/allegheny/fingertree/Measure.scala
+++ src/main/scala/edu/allegheny/fingertree/Measure.scala
@@ -1,14 +1,82 @@
 package edu.allegheny.fingertree
 
-/** Trait for a measure on a [[FingerTree]]
+/** Trait for a measure on a [[FingerTree]].
+  *
+  * A measure on a [[FingerTree]] is essentially a `Monoid`.
+  *
+  * A monoid is a semigroup with an identity. A monoid is a specialization of a
+  * semigroup, so its operation must be associative. Additionally,
+  * `combine(x, empty) == combine(empty, x) == x`.
   *
   * @todo Can this just be represented with a `Monoid` from `cats`?
+  *
+  * Right now we are essentially writing our own `Monoid` typeclass so as to
+  * avoid using an external library. It might be better to just depend on
+  * either `scalaz` or `algebra` (I think `algebra` looks nicer but it's also
+  * experimental) to get `Monoid`s.
   *
   * Created by hawk on 3/14/16.
   */
 trait Measure[V, -A] {
 
+  /** Return the identity measure.
+    *
+    * @todo should this be named `empty` by analogy with `Monoid`,
+    *       `identity`, or `zero`?
+    *
+    * @return the identity measure
+    */
+  def empty: V
+
+  /** Apply the measure to a `V`
+    *
+    * @param a
+    * @return
+    */
   def measure(a: A): V
+
+  /** Combine two measures.
+    *
+    * This implements the monoid multiplication (⊗) morphism.
+    *
+    * @param a
+    * @param b
+    * @return
+    */
+  def combine(a: V, b: V): V
+
+  /** The combine (⊗) operator.
+    *
+    * @param a
+    * @param b
+    * @return
+    */
+  @inline final def |+|(a: V, b: V): V = combine(a, b)
+
+  /** The combine (⊗) operator for varargs.
+    *
+    * @param xs
+    * @return
+    */
+  @inline final def |+|(xs: V*) = xs reduce |+|
+
+  /** The combine (⊗) operator for people who like using crazy unicode
+    * symbols in their code.
+    *
+    * @param a
+    * @param b
+    * @return
+    */
+  @inline final def ⊗(a: V, b: V): V  = combine(a, b)
+
+  /** The combine (⊗) operator for varargs, for people who like using crazy
+    * unicode symbols in their code.
+    *
+    * @param xs
+    * @return
+    */
+  @inline final def ⊗(xs: V*) = xs reduce ⊗
+
 
   @inline final def apply(a: A): V = measure(a)
 
