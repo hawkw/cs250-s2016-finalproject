fdbbb1192119d23cdd4d8dd86ef9a82424f5c90e
Aubrey Collins <aubrey@aubreypc.com>
2016-04-12 15:56:00 -0400

    did something?

--- src/main/scala/edu/allegheny/onion/simulation/Simulation.scala
+++ src/main/scala/edu/allegheny/onion/simulation/Simulation.scala
@@ -1,12 +1,20 @@
 package edu.allegheny.onion.simulation
-import edu.allegheny.graph.edgeWeighted
+import edu.allegheny.graph.edgeWeighted.Undigraph
 
 /**
   * Created by hawk on 4/12/16.
   */
 object Simulation {
 
-  val network: edgeWeighted.Digraph[Peer, Double]
-    = ???
+  val network: Undigraph[Peer, Double]
+    = new Undigraph()
+
+  val p1 = network.node(new Peer())
+  val p2 = network.node(new Peer())
+  val p3 = network.node(new Peer())
+
+  p1.connectTo(p2, 0.5)
+  p2.connectTo(p3, 0.75)
+  p3.connectTo(p1, 0.25)
 
 }
