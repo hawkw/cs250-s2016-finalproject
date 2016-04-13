494af1dc0697dd57a033c64615d4a418a329bfd4
Hawk Weisman <hi@hawkweisman.me>
2016-04-13 10:54:47 -0400

    [sim] Network is a type alias for Undigraph

--- src/main/scala/edu/allegheny/onion/simulation/package.scala
+++ src/main/scala/edu/allegheny/onion/simulation/package.scala
@@ -1,9 +1,11 @@
 package edu.allegheny.onion
+import edu.allegheny.graph.edgeWeighted
 
 /**
   * Created by hawk on 4/12/16.
   */
 package object simulation {
   type Link = (Peer, Double)
+  type Network = edgeWeighted.Undigraph[Peer, Double]
 
 }
