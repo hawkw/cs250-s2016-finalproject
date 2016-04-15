fc60120b5723ab81ccc41a174c198d0615f13a1a
Hawk Weisman <hi@hawkweisman.me>
2016-04-15 13:50:45 -0400

    [test] Add operator tests for edge-weighted graphs

---
+++ src/test/scala/edu/allegheny/test/GraphSpec.scala
@@ -0,0 +1,279 @@
+package edu.allegheny.test
+
+import edu.allegheny.graph.{Graph, edgeWeighted, unweighted}
+import edu.allegheny.onion.simulation.{GraphGenerator, Peer}
+import org.scalatest.{Matchers, OptionValues, WordSpec}
+
+import scala.io.Source
+
+/**
+  * Created by hawk on 4/12/16.
+  */
+class GraphSpec
+extends WordSpec
+  with Matchers
+  // with NodeMatchers
+  with OptionValues {
+
+  "An unweighted, directed graph" when {
+    "using operators to create edges" should {
+      "create an edge from A to B using the ~> operator" in {
+        val g = new unweighted.Digraph[Int]
+        val a = g node 1
+        val b = g node 2
+        a ~> b
+        a.hasEdgeTo(b) shouldBe true
+        b.hasEdgeTo(a) shouldBe false
+      }
+      "create a bidirectional edge between A and B using the <~> operator" in {
+        val g = new unweighted.Digraph[Int]
+        val a = g node 1
+        val b = g node 2
+        a <~> b
+        a.hasEdgeTo(b) shouldBe true
+        b.hasEdgeTo(a) shouldBe true
+      }
+    }
+    "using the ~>? operator to test for edges" should {
+      "return true when A has an edge to B" in {
+        val g = new unweighted.Digraph[Int]
+        val a = g node 1
+        val b = g node 2
+        a.connectTo(b)
+        a ~>? b shouldBe true
+      }
+      "return false when A does not have an edge to B" in {
+        val g = new unweighted.Digraph[Int]
+        val a = g node 1
+        val b = g node 2
+        a ~>? b shouldBe false
+      }
+      "return false when B has an edge to A but A doesn't have one to B" in {
+        val g = new unweighted.Digraph[Int]
+        val a = g node 1
+        val b = g node 2
+        b.connectTo(a)
+        a ~>? b shouldBe false
+      }
+    }
+    "using the <~? operator to test for edges" should {
+      "return true when B has an edge to A" in {
+        val g = new unweighted.Digraph[Int]
+        val a = g node 1
+        val b = g node 2
+        b.connectTo(a)
+        a <~? b shouldBe true
+      }
+      "return false when A does not have an edge to B" in {
+        val g = new unweighted.Digraph[Int]
+        val a = g node 1
+        val b = g node 2
+        a <~? b shouldBe false
+      }
+      "return false when B has an edge to A but A doesn't have one to B" in {
+        val g = new unweighted.Digraph[Int]
+        val a = g node 1
+        val b = g node 2
+        a.connectTo(b)
+        a <~? b shouldBe false
+      }
+    }
+  }
+
+  "An unweighted, undirected graph" when {
+    "using operators to create edges" should {
+      "create an edge from A to B using the ~> operator" in {
+        val g = new unweighted.Undigraph[Int]
+        val a = g node 1
+        val b = g node 2
+        a ~> b
+        a.hasEdgeTo(b) shouldBe true
+        b.hasEdgeTo(a) shouldBe true
+      }
+      // "create a bidirectional edge between A and B using the <~> operator" in {
+      //   val g = new unweighted.Undigraph[Int]
+      //   val a = g node 1
+      //   val b = g node 2
+      //   a <~> b
+      //   a.hasEdgeTo(b) shouldBe true
+      //   b.hasEdgeTo(a) shouldBe true
+      // }
+    }
+    "using the ~>? operator to test for edges" should {
+      "return true when A has an edge to B" in {
+        val g = new unweighted.Undigraph[Int]
+        val a = g node 1
+        val b = g node 2
+        a.connectTo(b)
+        a ~>? b shouldBe true
+      }
+      "return false when A does not have an edge to B" in {
+        val g = new unweighted.Undigraph[Int]
+        val a = g node 1
+        val b = g node 2
+        a ~>? b shouldBe false
+      }
+      "return true when the connection was created from B" in {
+        val g = new unweighted.Undigraph[Int]
+        val a = g node 1
+        val b = g node 2
+        b.connectTo(a)
+        a ~>? b shouldBe true
+      }
+    }
+    "using the <~? operator to test for edges" should {
+      "return true when B has an edge to A" in {
+        val g = new unweighted.Undigraph[Int]
+        val a = g node 1
+        val b = g node 2
+        b.connectTo(a)
+        a <~? b shouldBe true
+      }
+      "return false when A does not have an edge to B" in {
+        val g = new unweighted.Undigraph[Int]
+        val a = g node 1
+        val b = g node 2
+        a <~? b shouldBe false
+      }
+      "return true when the connection was created from B" in {
+        val g = new unweighted.Undigraph[Int]
+        val a = g node 1
+        val b = g node 2
+        a.connectTo(b)
+        a <~? b shouldBe true
+      }
+    }
+  }
+
+  "An edge weighted, directed graph" when {
+    "using operators to create edges" should {
+      "create an edge from A to B using the ~> operator" in {
+        val g = new edgeWeighted.Digraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        a ~> (b, 0.5)
+        a.hasEdgeTo(b) shouldBe true
+        b.hasEdgeTo(a) shouldBe false
+      }
+      "create a bidirectional edge between A and B using the <~> operator" in {
+        val g = new edgeWeighted.Digraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        a <~> (b, 0.5)
+        a.hasEdgeTo(b) shouldBe true
+        b.hasEdgeTo(a) shouldBe true
+      }
+    }
+    "using the ~>? operator to test for edges" should {
+      "return true when A has an edge to B" in {
+        val g = new edgeWeighted.Digraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        a.connectTo(b, 0.5)
+        a ~>? b shouldBe true
+      }
+      "return false when A does not have an edge to B" in {
+        val g = new edgeWeighted.Digraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        a ~>? b shouldBe false
+      }
+      "return false when B has an edge to A but A doesn't have one to B" in {
+        val g = new edgeWeighted.Digraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        b.connectTo(a, 0.5)
+        a ~>? b shouldBe false
+      }
+    }
+    "using the <~? operator to test for edges" should {
+      "return true when B has an edge to A" in {
+        val g = new edgeWeighted.Digraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        b.connectTo(a, 0.5)
+        a <~? b shouldBe true
+      }
+      "return false when A does not have an edge to B" in {
+        val g = new edgeWeighted.Digraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        a <~? b shouldBe false
+      }
+      "return false when B has an edge to A but A doesn't have one to B" in {
+        val g = new edgeWeighted.Digraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        a.connectTo(b, 0.5)
+        a <~? b shouldBe false
+      }
+    }
+  }
+
+  "An edgeWeighted, undirected graph" when {
+    "using operators to create edges" should {
+      "create an edge from A to B using the ~> operator" in {
+        val g = new edgeWeighted.Undigraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        a ~> (b, 0.5)
+        a.hasEdgeTo(b) shouldBe true
+        b.hasEdgeTo(a) shouldBe true
+      }
+      // "create a bidirectional edge between A and B using the <~> operator" in {
+      //   val g = new edgeWeighted.Undigraph[Int, Double]
+      //   val a = g node 1
+      //   val b = g node 2
+      //   a <~> b
+      //   a.hasEdgeTo(b) shouldBe true
+      //   b.hasEdgeTo(a) shouldBe true
+      // }
+    }
+    "using the ~>? operator to test for edges" should {
+      "return true when A has an edge to B" in {
+        val g = new edgeWeighted.Undigraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        a.connectTo(b, 0.5)
+        a ~>? b shouldBe true
+      }
+      "return false when A does not have an edge to B" in {
+        val g = new edgeWeighted.Undigraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        a ~>? b shouldBe false
+      }
+      "return true when the connection was created from B" in {
+        val g = new edgeWeighted.Undigraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        b.connectTo(a, 0.5)
+        a ~>? b shouldBe true
+      }
+    }
+    "using the <~? operator to test for edges" should {
+      "return true when B has an edge to A" in {
+        val g = new edgeWeighted.Undigraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        b.connectTo(a, 0.5)
+        a <~? b shouldBe true
+      }
+      "return false when A does not have an edge to B" in {
+        val g = new edgeWeighted.Undigraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        a <~? b shouldBe false
+      }
+      "return true when the connection was created from B" in {
+        val g = new edgeWeighted.Undigraph[Int, Double]
+        val a = g node 1
+        val b = g node 2
+        a.connectTo(b, 0.5)
+        a <~? b shouldBe true
+      }
+    }
+  }
+
+
+}
