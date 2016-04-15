cde679dd62769d5b7fb6a12c888319b88109fc59
Hawk Weisman <hi@hawkweisman.me>
2016-04-15 15:01:26 -0400

    [sim] Improve coding style slightly in GraphGen

--- src/main/scala/edu/allegheny/onion/simulation/GraphGenerator.scala
+++ src/main/scala/edu/allegheny/onion/simulation/GraphGenerator.scala
@@ -1,5 +1,6 @@
 package edu.allegheny.onion.simulation
 import scala.io.Source
+import scala.language.postfixOps
 
 /** Generates graphs from the Algs4 data file format
   *
@@ -15,13 +16,15 @@ object GraphGenerator {
 		// read file as a sequence of strings
 		val lines: Seq[String] = source.getLines.toIndexedSeq
 		// the first line in the input file is the number of nodes
-		val numNodes = lines.head.toInt
+		val numNodes = lines.headOption map (_ toInt) getOrElse 0
 		// create an array to store the new nodes and fill it with empty nodes
 		val nodes: Seq[graph.Node]
-			= for (i <- 0 until numNodes) yield graph.node(new Peer)
+			= for { i <- 0 until numNodes } yield graph.node(new Peer)
+
 		//parse rest of file and make connections
-		for (ln <- lines.drop(2) ) {
-			val Array(to, from, weight) = ln.split(' ')
+		for { ln <- lines.drop(2)
+			    Array(to, from, weight) = ln.split(' ')
+		} {
 			nodes(to.toInt).connectTo(nodes(from.toInt), weight.toDouble)
 		}
 
